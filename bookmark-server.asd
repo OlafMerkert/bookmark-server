b38dab4bc814d8667adb5eb6cdf5fd9dd0ec4784
Olaf Merkert <olaf@m-merkert.de>
2014-01-17 19:02:46 +0100

    allow unhiding and unhiding of the actual url behind the bookmark

--- bookmark-server.asd
+++ bookmark-server.asd
@@ -3,7 +3,10 @@
                web-utils
                hunchentoot cl-who
                clsql clsql-sqlite3
-               split-sequence)
+               split-sequence
+               css-lite
+               ;; uiop
+               parenscript)
   :serial t
   :components ((:file "bookmarks")
                (:file "web-interface")))
--- web-interface.lisp
+++ web-interface.lisp
@@ -1,11 +1,16 @@
+(in-package :cl-user)
+
 (defpackage :bookmark-web-interface
   (:nicknames :bm-web)
-  (:use :cl :ol :iterate :web-utils
-        :hunchentoot :cl-who)
+  (:shadowing-import-from :parenscript :this :in)
+  (:use :cl :ol :web-utils
+        :hunchentoot :cl-who
+        :parenscript)
   (:export))
 
 (in-package :bookmark-web-interface)
 
+(setf hunchentoot:*catch-errors-p* nil)
 
 (eval-when (:load-toplevel :execute)
   (register-web-application "Bookmark Server" "/bookmarks"))
@@ -51,13 +56,15 @@
 
 (define-easy-handler (bookmarks-list :uri "/bookmarks/list") ()
   (html/document (:title "Bookmarks"
-                         :style "/bookmarks/style.css")
+                         :style "/bookmarks/style.css"
+                         :script "/jquery.min.js"
+                         :script "/bookmarks/logic.js")
     (:h1 "Bookmarks")
     ;; todo Form for creating new bookmarks
-    (:form :name "bookmark-new" :onsubmit "false"
-           (:input :id "bookmark-id" :type "hidden" :value "")
-           (:input :id "bookmark-title" :type "text" :value "")
-           (:input :id "bookmark-url" :type "text" :value "")
+    (:form :name "bookmarkNew"
+           (:input :id "bookmarkId" :type "hidden" :value "")
+           (:input :id "bookmarkTitle" :type "text" :value "")
+           (:input :id "bookmarkUrl" :type "text" :value "")
            (:button :type "submit"
                     "New"))
     ;; todo List of present bookmarks
@@ -65,7 +72,34 @@
      (let (odd)
        (dolist (bm (bm:all-bookmarks))
          (htm (:li :id (bm:id bm)
-                   :class (if (notf odd) "odd" "even")
+                   :class (if (notf odd) "bookmark odd" "bookmark even")
                    (:a :href (bm:url bm) (esc (bm:title bm)))
                    (:br)
                    (:span :class "hidden" (esc (bm:url bm))))))))))
+
+(defmacro/ps @@ (&rest args)
+    `(chain ,@args))
+
+(defmacro/ps $! (obj handler args &body body)
+  "Install an event handler using jQuery on the solected object."
+  `(@@ ($ ,obj) (,handler (lambda ,args ,@body))))
+
+(define-easy-handler (bookmarks-js :uri "/bookmarks/logic.js") ()
+  (setf (hunchentoot:content-type*) "text/javascript")
+  (ps
+    ($! document ready ()
+      ;; hiding/unhiding url of bookmark
+      ($! ".bookmark" click ()
+        (let ((verbose-url (@@ ($ this) (find "span"))))
+          (if (@@ verbose-url (has-class "hidden"))
+              (@@ verbose-url (remove-class "hidden"))
+              (@@ verbose-url (add-class "hidden"))
+              ))))))
+
+(define-easy-handler (bookmarks-css :uri "/bookmarks/style.css") ()
+  (setf (hunchentoot:content-type*) "text/css")
+  (css-lite:css
+    ((".odd") (:background "#f1f6fe"))
+    ((".even") (:background "#f2f4f5"))
+    ((".hidden") (:display "none"))))
+

bfd80d1210262bc3d13fb6ddaf2aa92c5fbc1e37
Olaf Merkert <olaf@m-merkert.de>
2014-01-29 18:51:22 +0100

    move general parenscript macros to web-utils

--- web-interface.lisp
+++ web-interface.lisp
@@ -228,26 +228,6 @@
     (:p "Use [p] and [n] keys to select any entry.")))
 
 ;;; todo move generally useful parenscript macros to some utility collection
-(ew
-(defmacro/ps @@ (&rest args)
-    `(chain ,@args))
-
-(defmacro/ps $! (obj handler args &body body)
-  "Install an event handler using jQuery on the solected object."
-  `(@@ ($ ,obj) (,handler (lambda ,args ,@body)))))
-
-(defun lisp->camelcase (symbol &optional classp)
-  (let ((capit (string-capitalize (mkstr symbol))))
-    (remove #\- (if classp capit (string-downcase capit :start 0 :end 1)))))
-
-(defmacro/ps form-value (id &optional (value nil value-p))
-  `(@@ ($ ,(mkstr "#" (lisp->camelcase id))) (val ,@(if value-p (list value)))))
-
-(defmacro/ps form-bind (bindings &body body)
-  `(let ,(mapcar #`(,a1 (form-value ,a1)) bindings)
-     ,@body))
-
-
 (define-easy-handler (bookmarks-js :uri "/bookmarks/logic.js") ()
   (setf (hunchentoot:content-type*) "text/javascript")
   (ps
